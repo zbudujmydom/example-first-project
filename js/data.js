63682a8b16579d55e0798d0c59c06f7d768b4f67
Tomek Sochacki <zbudujmydompl@gmail.com>
2024-05-11 11:09:45 +0200

    more info about data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,14 @@
 const incomes = [];
 
 const getIncomes = () => {
+  // in real application we could use some deep copy of this array because
+  // we want to reveal raw source data, we want to modify this array only
+  // by means of our dedicated functions like addIncome, removeIncome etc.
+  // but in this project you don't have to use real deep copy, we can use only
+  // this simple function.
+
+  // The simplest way to do deep copy for that object is for instance:
+  // return JSON.parse(JSON.stringify(incomes))
   return incomes;
 };
 
